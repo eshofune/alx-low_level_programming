a041fc4c2a1eb2ac19481d24f85c134002075ab7
eshofune <precioususman277@gmail.com>
2023-03-17 09:01:49 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/* more headers goes there */
+/* betty style doc for function main goes there */
+/**
+* main - Entry point
+* Return: Always 0 (Successful)
+*/
+int main(void)
+{
+int i;
+char j;
+for (i = 0; i < 10; i++)
+putchar(i + '0');
+for (j = 'a'; j <= 'f'; j++)
+putchar(j);
+putchar('\n');
+return (0);
+}
