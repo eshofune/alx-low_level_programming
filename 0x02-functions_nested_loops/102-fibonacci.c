cc393777109799f840338f6afccc2c43bbd6cf9e
eshofune <precioususman277@gmail.com>
2023-03-22 17:40:16 -0700

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -6,21 +6,18 @@
 int main(void)
 {
 int i = 0;
-long j = 1, k = 2;
+long int a = 0, b = 1, next;
 while (i < 50)
 {
-if (i == 0)
-printf("%ld", j);
-else if (i == 1)
-printf("%ld", k);
-else
+next = a + b;
+a = b;
+printf("%lu", next);
+if (i < 49)
 {
-k += j;
-j = k - j;
-printf(", %ld", k);
+printf(", ");
 }
-++i;
+i++;
 }
-printf("\n");
+putchar('\n');
 return (0);
 }
