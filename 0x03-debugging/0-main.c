a8455bc75ea516da53ab905e28482a49c7cbb4de
eshofune <precioususman277@gmail.com>
2023-03-23 17:59:46 -0700

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -4,10 +4,13 @@
 * positive_or_negative: prints either positive or negative number
 * Return: Always 0 (Successful)
 */
+
 int main(void)
 {
 int i;
+
 i = 0;
 positive_or_negative(i);
+
 return (0);
 }
