3cb521c4fd62145ce3e95def88043999932e3724
eshofune <precioususman277@gmail.com>
2023-04-27 06:45:21 -0700

    100-first.c

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+* free_list - frees a linked list
+* @head: list_t list to be freed
+*/
+
+void free_list(list_t *head)
+{
+list_t *temp_node;
+while (head)
+{
+temp_node = head->next;
+free(head->str);
+free(head);
+head = temp_node;
+}
+}
